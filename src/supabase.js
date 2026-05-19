fd8bd359a95e1138c1dfff9a26fae07aad405b59
Sai Ganesh <yourgmail@gmail.com>
2026-05-19 08:55:57 +0530

    fixed supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ladwfnabjitolymkyyek.supabase.co/rest/v1/'
+const supabaseUrl = 'https://ladwfnabjitolymkyyek.supabase.co'
+
 const supabaseKey = 'sb_publishable_tzJiRPYHnXSUMklPduPTiQ_2xewVxoe'
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
